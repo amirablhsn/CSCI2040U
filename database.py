28c8b773345542ca8c7db6639bf18752ab4bf58e
Christine <chtran93@gmail.com>
2025-01-24 16:32:16 -0500

    Add fetch, add, delete, and validate functions

--- database.py
+++ database.py
@@ -21,3 +21,50 @@ def init_db(dummy_data=True):
             print("Database initialized")
 
 init_db()
+
+# Returns true if name and desc are non-empty strings
+def validate_input(name, description):
+    return name.strip() != "" and description.strip() != ""
+
+# Reads csv file, returns database entires in a list of dicts
+def fetch_all():
+    with open(csv_file, mode="r", newline="") as data_file:
+        reader = csv.DictReader(data_file)
+        return list(reader)
+
+def add(name, description):
+    if not validate_input(name, description):
+        return
+    
+    data = fetch_all()
+
+    # Auto increments id
+    if len(data) == 0:
+        id = 1
+    else: 
+        id = int(data[-1]["ID"]) + 1
+    
+    # appends new data entry
+    with open(csv_file, mode="a", newline="") as data_file:
+        writer = csv.writer(data_file)
+        writer.writerow([id, name, description])
+
+# deletes data entry by id, returns true if successful
+def delete(id):
+    data = fetch_all()
+    success = False
+
+    for entry in data:
+        if entry["ID"] == str(id):
+            data.remove(entry)
+            success = True
+            break
+    
+    # Only updates file if entry is found and removed
+    if success:
+        with open(csv_file, mode="w", newline="") as data_file:
+            writer = csv.DictWriter(data_file, field_names)
+            writer.writeheader()
+            writer.writerows(data)
+
+    return success
