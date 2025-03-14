17951000c57778021edce5fa2d18d2507336dc2b
Christine <christine.tran@ontariotechu.net>
2025-03-14 14:03:09 -0400

    add ability to delete vehicle

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("add/", views.add, name="add"),
     path("vehicle/<int:id>/", views.details, name="details"),
     path('edit/<int:id>/', views.edit, name='edit'),
+    path("delete/<int:id>/", views.delete, name="delete"),
 ]
 
 if settings.DEBUG:
